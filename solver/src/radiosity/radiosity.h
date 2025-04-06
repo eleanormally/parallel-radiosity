604d0f9d41a31e52fd1bcaab81119f97171fb214
Eleanor Olson <eleanorkolson@gmail.com>
2025-04-06 13:37:41 -0400

    updated input type for radiosity solving

--- solver/src/radiosity/radiosity.h
+++ solver/src/radiosity/radiosity.h
@@ -1,6 +1,11 @@
-
 #include <cstddef>
-typedef struct FormFactor {
-  float* array;
-  size_t size;
-} FormFactor;
+#include <vector>
+#include "../utils/vectors.h"
+using std::size_t;
+using std::vector;
+
+typedef struct RadiosityInput {
+  vector<vector<float>> formFactors;
+  vector<Vec3f>         emmittence;
+  vector<Vec3f>         diffuse;
+} RadiosityInput;
